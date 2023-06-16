0646de02ecbda236fa60301de07e9033fe63f66f
Elchin Huseynli <elchin.kv@gmail.com>
2023-06-16 11:42:31 +0200

    remove stats

--- dist/particles.js
+++ dist/particles.js
@@ -114,24 +114,5 @@
     },
     retina_detect: true
   });
-  var count_particles;
-  var stats;
-  var update;
-  stats = new Stats();
-  stats.setMode(0);
-  stats.domElement.style.position = "absolute";
-  stats.domElement.style.left = "0px";
-  stats.domElement.style.top = "0px";
-  document.body.appendChild(stats.domElement);
-  count_particles = document.querySelector(".js-count-particles");
-  update = function() {
-    stats.begin();
-    stats.end();
-    if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
-      count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
-    }
-    requestAnimationFrame(update);
-  };
-  requestAnimationFrame(update);
 })();
 //# sourceMappingURL=particles.js.map
